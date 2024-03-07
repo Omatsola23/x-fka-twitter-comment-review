d74e8ab59ac3b303dac6487f40a172b32e6d077d
omatsolaeyewu-edero <omatsolajennings@yahoo.com>
2024-03-07 15:53:13 +0100

    Initial commit

--- x.py
+++ x.py
@@ -12,12 +12,12 @@ vectorizer=py.load("vector.pkl")
 user_input=st.text_input('Enter Comment')
 predictions=vectorizer.transform([user_input])
 predictx=model.predict(predictions)
-st.button("ENTER")
-if predictx==1:
-  st.write("Postive")
-elif predictx==0:
-  st.write("Neutral")
-else:
-  st.write("Negative")
+if st.button("ENTER"):
+  if predictx==1:
+    st.write("Postive")
+  elif predictx==0:
+    st.write("Neutral")
+  else:
+    st.write("Negative")
 st.markdown("""<div style='text-align: center;'><p style='font-size: small;'>this app used machine learning to train itself with the dataset and accuracy is 86%</p></div>""", unsafe_allow_html=True)
 st.markdown("""<div style='text-align: center;'><p style='font-size: small;'>by Omatsola</p></div>""", unsafe_allow_html=True)
